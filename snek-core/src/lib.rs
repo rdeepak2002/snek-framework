f5415f8d984501544b97c6eec90257858ed3c65c
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-08-28 02:19:15 -0400

    Better error handling
    Took 12 minutes

--- snek-core/src/lib.rs
+++ snek-core/src/lib.rs
@@ -1,15 +1,5 @@
-use gc::{Finalize, Trace};
-use rustpython_vm::builtins::{PyDictRef, PyIntRef};
-use rustpython_vm::convert::{ToPyObject, ToPyResult};
-use rustpython_vm::function::{ArgMapping, FuncArgs, IntoFuncArgs, OptionalArg};
-use rustpython_vm::protocol::PyNumber;
-use rustpython_vm::scope::Scope;
-use rustpython_vm::{
-    compiler, pyclass, pymodule,
-    types::{Constructor, GetDescriptor, Unconstructible},
-    vm, AsObject, Interpreter, PyObject, PyObjectRef, PyPayload, PyResult, TryFromBorrowedObject,
-    VirtualMachine,
-};
+use rustpython_vm::builtins::PyBaseExceptionRef;
+use rustpython_vm::{compiler, pymodule, Interpreter, PyResult, VirtualMachine};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -20,6 +10,26 @@ extern "C" {
 #[wasm_bindgen]
 pub fn init_logger() {
     wasm_logger::init(wasm_logger::Config::new(log::Level::Debug));
+}
+
+pub fn get_error_content(e: &PyBaseExceptionRef, vm: &VirtualMachine) -> String {
+    let mut error_content = String::new();
+    let py_err_arg = e.get_arg(0);
+    if py_err_arg.is_some() {
+        let py_err_str = py_err_arg.unwrap().str(vm);
+        if py_err_str.is_ok() {
+            let py_err = py_err_str.unwrap();
+            log::error!("{}", py_err);
+            error_content += &*String::from(format!("{}", py_err));
+        }
+    }
+    let traceback = e.traceback();
+    if traceback.is_some() {
+        let line_number = traceback.unwrap().lineno;
+        log::error!("line {}", line_number);
+        error_content += &*String::from(format!(", line {}", line_number));
+    }
+    error_content
 }
 
 #[wasm_bindgen]
@@ -44,7 +54,10 @@ pub fn python_to_html(code: &str) -> String {
             .compile(code, compiler::Mode::BlockExpr, source_path.to_owned())
             .map_err(|err| vm.new_syntax_error(&err, None));
         if code_obj.is_err() {
-            website_code = String::from("<div style=\"color: red;\">Error</div>");
+            let e = code_obj.as_ref().err().unwrap();
+            let error_content = get_error_content(e, vm);
+            website_code =
+                String::from(format!("<div style=\"color: red;\">{error_content}</div>"));
             return;
         }
         let code_obj = code_obj.unwrap();
@@ -53,50 +66,31 @@ pub fn python_to_html(code: &str) -> String {
                 let args = vec![];
                 let res = render.call(args, vm);
                 if let Err(..) = res {
-                    let mut error_content = String::new();
                     let e = res.as_ref().unwrap_err();
-                    let py_err_arg = e.get_arg(0);
-                    if py_err_arg.is_some() {
-                        let py_err_str = py_err_arg.unwrap().str(vm);
-                        if py_err_str.is_ok() {
-                            let py_err = py_err_str.unwrap();
-                            log::error!("{}", py_err);
-                            error_content += &*String::from(format!("{}", py_err));
-                        }
-                    }
-                    let traceback = e.traceback();
-                    if traceback.is_some() {
-                        let line_number = traceback.unwrap().lineno;
-                        log::error!("line {}", line_number);
-                        error_content += &*String::from(format!(", line {}", line_number));
-                    }
+                    let error_content = get_error_content(e, vm);
                     website_code =
                         String::from(format!("<div style=\"color: red;\">{error_content}</div>"));
+                    return;
                 } else {
                     website_code = String::from(res.as_ref().unwrap().str(vm).unwrap().as_str());
-                    log::debug!("{}", website_code);
+                    return;
                 }
             }
         });
         if res.is_err() {
+            let e = res.as_ref().unwrap_err();
+            let error_content = get_error_content(e, vm);
             website_code =
-                String::from(format!("<div style=\"color: red;\">Fatal exception</div>"));
+                String::from(format!("<div style=\"color: red;\">{error_content}</div>"));
+            return;
         }
     });
+    log::debug!("{}", website_code);
     String::from(website_code)
 }
 
 #[pymodule]
 pub(crate) mod snek_core {
-    use std::any::Any;
-
-    use rustpython_vm::builtins::{PyGenericAlias, PyStr, PyStrInterned, PyTypeRef};
-    use rustpython_vm::protocol::PyNumberMethods;
-    use rustpython_vm::types::AsNumber;
-    use rustpython_vm::{
-        builtins::PyList, convert::ToPyObject, Py, PyObjectRef, TryFromBorrowedObject,
-    };
-
     use super::*;
 
     #[pyfunction]
