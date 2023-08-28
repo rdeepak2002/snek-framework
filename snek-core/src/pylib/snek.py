dc96efa9985a5c9339235da1b709d0f0193e28cc
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-08-28 02:05:18 -0400

    Fix tabs for snake demo
    Took 14 minutes

--- snek-core/src/pylib/snek.py
+++ snek-core/src/pylib/snek.py
@@ -1,5 +1,5 @@
 from snek_core import add_two_internal
 
-
+# TODO: this is not being used anywhere, but it could be imported in demo.py using 'from snek import add_two'
 def add_two(num):
     return add_two_internal(num)
