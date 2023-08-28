56db710d7ebc610438b02f03fae94b497f563237
rdeepak2002 <rdeepak2002@gmail.com>
2023-08-28 06:13:26 +0000

    Deploying to gh-pages from @ rdeepak2002/snek-framework@dc96efa9985a5c9339235da1b709d0f0193e28cc 🚀

--- demo.py
+++ demo.py
@@ -1,11 +1,12 @@
-class Foo:
-    def render(self):
-        x = 12
-        y = 5.5
-        return f"<div>" \
-               f"<h1>Header</h1>" \
-               f"<p>foo bar {x + y}</p>" \
-               f"</div>"
+class SamplePage:
+  def render(self):
+    x = 12
+    y = 5.5
+    return f"<div>" \
+           f"<h1>Header</h1>" \
+           f"<p>Sample paragraph</p>" \
+           f"<p>Sample Python computation {x + y}</p>" \
+           f"</div>"
 
 
-Foo()
+SamplePage()
