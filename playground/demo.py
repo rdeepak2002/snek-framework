21a688f0f014d1a692ef6ccedcb374bc9bf72a27
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-08-28 02:28:20 -0400

    Better demo
    Took 9 minutes

--- playground/demo.py
+++ playground/demo.py
@@ -1,11 +1,19 @@
 class SamplePage:
-  def render(self):
+  def fib(self, n: int) -> int:
+    if n <= 2:
+      return 1
+    else:
+      return self.fib(n - 1) + self.fib(n - 2)
+
+  def render(self) -> str:
     x = 12
     y = 5.5
     return f"<div>" \
-           f"<h1>Header</h1>" \
-           f"<p>Sample paragraph</p>" \
-           f"<p>Sample Python computation {x + y}</p>" \
+           f"<h1 style='color: blue;'>Header</h1>" \
+           f"<p style='color: red;'>Sample paragraph</p>" \
+           f"<p>{x} + {y} = {x + y}</p>" \
+           f"<p>{x} / {y} = {x / y}</p>" \
+           f"<p>The 10th fibonacci number is {self.fib(10)}</p>" \
            f"</div>"
 
 
