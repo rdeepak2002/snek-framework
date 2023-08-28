9f26d7e6478876e7ac3ee843013b3c7e1cc4dfaa
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-08-28 00:29:36 -0400

    Use wasm logging
    Took 3 minutes

--- snek-core/src/lib.rs
+++ snek-core/src/lib.rs
@@ -6,6 +6,11 @@ extern "C" {
 }
 
 #[wasm_bindgen]
+pub fn init_logger() {
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Debug));
+}
+
+#[wasm_bindgen]
 pub fn python_to_html(code: &str) -> String {
     log::debug!("Executing Python code {}", code);
     let website_code = r#"
